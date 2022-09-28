e2c6527cd21186ab409153cf49fd49d487ed6c94
emmanuelsolis <emma.ruso@outlook.com>
2022-09-28 06:04:26 -0500

    Added a Carousel and show the servicesCards in Colimns

--- src/pages/ListServices.jsx
+++ src/pages/ListServices.jsx
@@ -1,75 +1,88 @@
-import React, { useState, useEffect } from 'react'
-import {Row, Col, Button, Divider, Typography, Carousel } from "antd";
-import { ServiceForm, ServiceCard,   } from '../components';
-import { serviceListWs } from '../services/cleanServices-ws';
-
+import React, { useState, useEffect } from "react";
+import { Row, Col, Button, Divider, Typography, Carousel } from "antd";
+import { ServiceForm, ServiceCard } from "../components";
+import { serviceListWs } from "../services/cleanServices-ws";
 
 const contentStyle = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  };
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79"
+};
 
 const ListServices = (props) => {
-    const [serviceCards, setserviceCards] = useState([]);
-    const [isCreate, setIsCreate] = useState(false);
-    const [beingCreated, setBeingCreated] = useState(false);
-  
-
-    const onChange = (currentSlide) => {
-        console.log(currentSlide);
-      };
+  const [serviceCards, setserviceCards] = useState([]);
+  const [isCreate, setIsCreate] = useState(false);
+  const [beingCreated, setBeingCreated] = useState(false);
+  const onChange = (currentSlide) => {
+      console.log(currentSlide);
+    };
     useEffect(() => {
         serviceListWs()
-            .then( res => {
-
-                setserviceCards(res.data.services)
-            })
-            .catch( err => {
-                console.log(err)
-            });
-    }, [beingCreated])
-    return(
-        <div>
-            <div>
-                <Typography.Title level={1}>Mis Servicios</Typography.Title>
-                <Divider orientation="center" />
-                {props.user.role === "Admin" && <Button type='primary' onClick={()=> setIsCreate(!isCreate)}>
-                    Crear un nuevo servicio
-                </Button>}
-                {isCreate && <ServiceForm
-                    beingCreated={beingCreated}
-                    setBeingCreated={setBeingCreated}
-                />}
-            </div>
-            <div className="cards">
+        .then((res) => {
+            setserviceCards(res.data.services);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, [beingCreated]);
+    return (
+    <div>
+      <div>
+        <Typography.Title level={1}>Mis Servicios</Typography.Title>
+        <Divider orientation="center" />
+        {props.user.role === "Admin" && (
+          <Button type="primary" onClick={() => setIsCreate(!isCreate)}>
+            Crear un nuevo servicio
+          </Button>
+        )}
+        {isCreate && (
+          <ServiceForm
+            beingCreated={beingCreated}
+            setBeingCreated={setBeingCreated}
+          />
+        )}
+      </div>
+      <div className="cards">
+        <div style={{ width: "100vh", height: "100vh" }}>
+          <Carousel autoplay afterChange={onChange}>
+            {serviceCards.map((service, idx) => {
+              return (
+                <div style={{ contentStyle, lineHeight: "300px" }}>
+                  <ServiceCard
+                    img={service.photoServiceUrl}
+                    name={service.typeService}
+                    price={service.price}
+                    description={service.description}
+                    deliverTime={service.deliverTime}
+                  />
+                  <h3 style={contentStyle}>{idx + 1}</h3>
+                </div>
+              );
+            })}
+          </Carousel>
+        </div>
+        <div className="cards">
                 <Row gutter={[40, 16]}>
-                    {serviceCards.map( service => {
-                        return(
-                            <Carousel afterChange={onChange}>
-                                <Col span={40}>
-                                    <ServiceCard 
+                    {serviceCards.map((service, idx) => {
+                        return (
+                            <Col span={8}>
+                                <ServiceCard
                                     img={service.photoServiceUrl}
                                     name={service.typeService}
                                     price={service.price}
                                     description={service.description}
                                     deliverTime={service.deliverTime}
-                                        
-                                    />
-                                    
-                                </Col>
-                            </Carousel>
-                        )
-                    })}    
+                                />
+                            </Col>
+                        );
+                    })}
                 </Row>
-            </div>
-
         </div>
-    )
+      </div>
+    </div>
+  );
+};
 
-  
-}
-
-export default ListServices
+export default ListServices;
